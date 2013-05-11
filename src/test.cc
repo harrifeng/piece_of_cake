2100d989a1739e550ed0349870077eb081e34ff1
Macair <harrifeng@gmail.com>
2013-05-11 19:51:33 +0800

    [tag] understanding the longest substring question by example

--- src/test.cc
+++ src/test.cc
@@ -11,9 +11,19 @@ int lengthOfLongestSubstring(string s) {
     memset(count, -1, 26 * sizeof(int));
     int len = 0;
     int max_len = 0;
+    for (int i = 0; i < 26; ++i) {
+        printf("%2c  ", 97+i);
+        // cout << count[i] << " ";
+    }
+    cout << endl;
 
     for (int i = 0; i < s.size(); ++i, ++len) {
         if (count[s[i] - 'a'] >= 0) {
+            for (int i = 0; i < 26; ++i) {
+                printf("%2d  ", count[i]);
+                // cout << count[i] << " ";
+            }
+            cout << endl << "--------------------------------------------------------------------------------------------------------" << endl;
             max_len = std::max(len, max_len);
             len = 0;
             i = count[s[i] - 'a'] + 1;
@@ -25,7 +35,18 @@ int lengthOfLongestSubstring(string s) {
 }
 
 int main(int argc, char *argv[]) {
-    cout << lengthOfLongestSubstring("abcabcbbbc") << endl;
+    string str = "thgpgwkostwfhsgapkzwmzorfefwofihmoz";
+    for (int i = 0; i < str.size(); ++i) {
+        printf("%2d  ", i % 10);
+    }
+    cout << endl;
+    for (int i = 0; i < str.size(); ++i) {
+        printf("%2c  ", str[i]);
+    }
+    cout << endl;
+    cout << "--------------------------------------------------------------------------------------------------------" << endl;
+    cout << "--------------------------------------------------------------------------------------------------------" << endl;
+    cout << lengthOfLongestSubstring(str) << endl;
     return 0;
 }
 
