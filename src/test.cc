ccc9def3604cfab151e7612cce69794fb4dc1fa3
harrifeng <harrifeng@gmail.com>
2013-05-19 18:45:59 +0800

    small pass for 4sum

--- src/test.cc
+++ src/test.cc
@@ -7,61 +7,77 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int> > threeSum(vector<int> &num) {
+    vector<vector<int> > fourSum(vector<int> &num, int target) {
         vector<vector<int> > ret;
+        if (num.size() < 4) {
+            return ret;
+        }
         std::sort(num.begin(), num.end());
         int i = 0;
-        for (i = 0; i < num.size(); ++i) {
-            // if the value is equal to previous one, skip it.
-            while (i > 0  && i < num.size() && num[i] == num[i-1]) {
-                i++;
-            }
-            int front = i + 1;
-            int back = num.size() - 1;
-            while (front < back) {
-                int sum = num[front] + num[back];
-                if ((sum + num[i]) == 0) {
-                    vector<int> pair;
-                    pair.push_back(num[i]);
-                    pair.push_back(num[front]);
-                    pair.push_back(num[back]);
-                    ret.push_back(pair);
-                    back--;
-                    front++;
-                    // if the (back--) and (front++) happen to be same with
-                    // their previous value, skip them.
-                    while ((front < back) && (num[back] == num[back+1]) &&
-                           (num[front] == num[front--])) {
-                        front++;
-                        back--;
-                    }
-                } else if ((sum + num[i]) < 0) {
-                    front++;
+        int j = num.size() - 1;
+            int count = 0;
+
+        while (j - i >= 3) {
+            int sum = target - (num[i] + num[j]) ;
+            // cout << "--------------" << endl;
+            // cout << num[i] << endl;
+            // cout << num[j] << endl;
+            // cout << sum << endl;
+            // cout << target << endl;
+            // cout << "--------------" << endl;
+            int beg = i + 1;
+            int end = j - 1;
+
+            while (beg < end) {
+                if ((num[beg] + num[end]) == sum) {
+                    vector<int> result;
+                    result.push_back(num[i]);
+                    result.push_back(num[beg]);
+                    result.push_back(num[end]);
+                    result.push_back(num[j]);
+                    ret.push_back(result);
+                    beg++;
+                    end--;
+                } else if ((num[beg] + num[end]) < sum) {
+                    beg++;
                 } else {
-                    back--;
+                    end--;
                 }
-            }//while
-        }//for
+            }
+            if (count == 0) {
+                i++;
+                count++;
+            } else if (count == 1) {
+                j--;
+                i--;
+                count++;
+            } else if (count == 2) {
+                i++;
+                count = 0;
+            }
+        }
+
         return ret;
     }
 };
 
 int main(int argc, char *argv[]) {
     vector<int> input;
+    input.push_back(1);
+    input.push_back(0);
     input.push_back(-1);
     input.push_back(0);
-    input.push_back(1);
+    input.push_back(-2);
     input.push_back(2);
-    input.push_back(-1);
-    input.push_back(-4);
 
     Solution* ss = new Solution();
-    vector<vector<int> > output = ss->threeSum(input);
+    vector<vector<int> > output = ss->fourSum(input, -1);
 
     for (int i = 0; i < output.size(); ++i) {
         cout << output[i][0] << " " ;
         cout << output[i][1] << " " ;
         cout << output[i][2] << " " ;
+        cout << output[i][3] << " " ;
         cout << endl;
     }
     return 0;
