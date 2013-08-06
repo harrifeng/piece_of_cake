9df9b904c10a939dc87bb34e2942aac94faf0aea
hfeng-air <harrifeng@gmail.com>
2013-08-06 23:43:27 +0800

    modify [leet] longest substring without repeating chars, with more logical way

--- src/leetcode/longest_substring_without_repeating_characters.cc
+++ src/leetcode/longest_substring_without_repeating_characters.cc
@@ -2,11 +2,9 @@
 // 注意事项:
 // 1) 在找最大值这种问题中, max_len可以用max_len = std:max(len, max_len)这
 //    种非常简洁的表达方式
-// 2) 如果把上面判断最大值的语句[max_len = std:max(len, max_len)]放到len++的
-//    后面,那么势必是没有错误的,但是这样效率就不好. 为了提高效率,我们一般把这个
-//    语句放到len被重置为0的时候,也就是下面例子的else分支中, 但是一定不要忘了,
-//    在return的时候,在判断一次max, 因为有可能字符串以最大长度不重复结束,那么就
+// 2) 一定不要忘了,在return的时候,在判断一次max, 因为有可能字符串以最大长度不重复结束,那么就
 //    压根不会进入到else分支
+// 3) memset(arr, value, sizeof(type) * size 真心难记.
 //
 ///////////////////////////////////////////////////////////////////////////////
 #include <cstdio>
@@ -21,33 +19,59 @@ using namespace std;
 class Solution {
  public:
     int lengthOfLongestSubstring(string s) {
-        int len = 0;
-        int max_len = 0;
-        int i = 0;
-        int hits[26];
-        memset(hits, -1, sizeof(int) * 26);
+	int len = 0;
+	int max_len = 0;
+	int index[256];
+	memset(index, -1, sizeof(int) * 256);
+	for (int i = 0; i < s.size(); i++) {
+	    if (index[s[i]] >= 0) {
+		i = index[s[i]] + 1;
+		memset(index, -1, sizeof(int) * 256);
+		max_len = std::max(max_len, len);
+		len = 0;
+	    }
+	    index[s[i]] = i;
+	    len++;
+	}
+	return std::max(max_len, len);
+    }
+};
 
-        while (i < s.size()) {
-            int prev_pos = hits[s[i] - 'a'];
-            if (prev_pos < 0) {
-                hits[s[i] - 'a'] = i;
-                len++;
-                i++;
-            } else {
-                memset(hits, -1, sizeof(int) * 26);
-                max_len = std::max(len, max_len);
-                len = 0;
-                i = prev_pos + 1;
-            }
-        }
-        // very easy to forget, they may never go to else
-        return std::max(len, max_len);
+class TestCase {
+    string inp;
+    int    exp;
+public:
+    TestCase(){}
+    TestCase(string input, int expect) : inp(input), exp(expect){}
+
+    void test_solution(Solution ss) {
+	cout << "\n|--Test Casing------------------------------------|" << endl;
+	cout << "|---Input String : \t[";
+	cout << inp << "]" << endl;
+	cout << "|--Expected-->\t" << exp << endl;
+	cout << "|--Output---->\t";
+	cout << ss.lengthOfLongestSubstring(inp) << endl;
     }
 };
 
 int main(int argc, char *argv[]) {
-    Solution* ss = new Solution();
+    Solution ss;
 
-    cout << ss->lengthOfLongestSubstring("abcabcbbbc") << endl;
+    string inp1 = "abcdabcd";
+    TestCase t1(inp1, 4);
+    t1.test_solution(ss);
+
+    string inp2 = "abcbaebcdef";
+    TestCase t2(inp2, 5);
+    t2.test_solution(ss);
+
+    string inp3 = "abcabcbb";
+    TestCase t3(inp3, 3);
+    t3.test_solution(ss);
+    
+	
+    // Solution* ss = new Solution();
+    // 
+    // cout << ss->lengthOfLongestSubstring("abcabcbbbc") << endl;
     return 0;
 }
