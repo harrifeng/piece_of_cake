b4ed0b7d1612d47999677d272d55f330287b97fb
hfeng-air <harrifeng@gmail.com>
2013-08-08 17:52:44 +0800

    test hardest longest palindrome

--- src/test.cc
+++ src/test.cc
@@ -1,44 +1,64 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
 
 using namespace std;
 
 class Solution {
 public:
-    void permuteHelper(vector<int> num, int i, vector<vector<int> > &result) {
-        if (i == num.size()) result.push_back(num);
-        else {
-            for (int j = i; j < num.size(); j++) {
-                swap(num[i], num[j]);
-                permuteHelper(num, i + 1, result);
-                swap(num[i], num[j]);
-            }
+    string pre_process(string s) {
+        int n = s.length();
+        if (n == 0) {
+            return "^$";
         }
-    }
+        string ret = "^";
+	for (int i = 0; i < n; i++) {
+	    ret.push_back('#');
+	    ret.push_back(s[i]);
+	}
+        ret += "#$";
+        return ret;
+    }    
+    string longestPalindrome(string s) {
+	string nstr = pre_process(s);
+	int nlen = nstr.size();
+	int *ptr = new int[nlen];
+	int max_id = 0;
+	int max_pos = 0;
 
-    vector<vector<int> > permute(vector<int> &num) {
-        vector<vector<int> > result;
-        permuteHelper(num, 0, result);
-        return result;
+	for (int i = 1; i < nlen - 1; i++) {
+	    int i_mirror = 2 * max_id - i;
+	    ptr[i] = (max_pos > i) ? min(max_pos -i, ptr[i_mirror]) : 0;
+
+	    while(nstr[i+1+ptr[i]] == nstr[i-1-ptr[i]]) {
+		ptr[i]++;
+	    }
+
+	    if (i + ptr[i] > max_pos) {
+		max_id = i;
+		max_pos = i + ptr[i];
+	    }
+	}
+	int maxlen = 0;
+	int centerIndex = 0;
+	for (int i = 1; i < nlen - 1; i++) {
+	    if (ptr[i] > maxlen) {
+		maxlen=ptr[i];
+		centerIndex = i;
+	    }
+	}
+	delete[] ptr;
+	return s.substr((centerIndex - 1 - maxlen) / 2, maxlen);
     }
+		    
 };
+    
 
 int main(int argc, char *argv[]) {
     Solution ss;
-    vector<int> vec;
-    vector<vector<int> > res;
-    
-    vec.push_back(1);
-    vec.push_back(2);
-    vec.push_back(3);
-    res = ss.permute(vec);
-
-    for (int i = 0; i < res.size(); i++) {
-	for(int j = 0; j < res[i].size(); j++) {
-	    cout << res[i][j];
-	}
-	cout << endl;
-    }
+    // string hh = ss.pre_process("wabccbw");
+    string hh = ss.longestPalindrome("abcddcbawwwwwwwwwwa");
+    cout << hh << endl;
     return 0;
 }
