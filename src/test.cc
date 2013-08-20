03d05514872387c00be2fd6c67a7079a045d5bb3
hfeng-air <harrifeng@gmail.com>
2013-08-20 22:06:37 +0800

    test parentheses

--- src/test.cc
+++ src/test.cc
@@ -1,47 +1,36 @@
+/////////////////////////////////////////////////////////////////////////////////
+// Given n pairs of parentheses, write a function to generate all combinations //
+// of well-formed parentheses.                                                 //
+//                                                                             //
+// For example, given n = 3, a solution set is:                                //
+//                                                                             //
+// "((()))", "(()())", "(())()", "()(())", "()()()"                            //
+/////////////////////////////////////////////////////////////////////////////////
+
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
-    void append_to_roman(int num, string &roman, char symbols[]) {
-        if (num == 0) {
-            return;
-        }
-        if (num <= 3) {
-            roman.append(num, symbols[0]);
-        } else if (num == 4) {
-            roman.append(1, symbols[0]);
-            roman.append(1, symbols[1]);
-        } else if (num <= 8) {
-            roman.append(1, symbols[1]);
-            roman.append(num-5, symbols[0]);
-        } else {
-            roman.append(1, symbols[0]);
-            roman.append(1, symbols[2]);
-        }
+    vector<string> generateParenthesis(int n) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        
     }
-
-    string intToRoman(int num) {
-        char symbol[9] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
-        string roman = "";
-        int scale = 1000;
-        for (int i = 6; i >= 0; i-=2) {
-            int digit = num / scale;
-            append_to_roman(digit, roman, symbol + i);
-            num = num % scale;
-            scale /= 10;
-        }
-        return roman;
-    }
-
 };
 
 int main(int argc, char *argv[])
 {
     Solution ss;
-    cout << ss.intToRoman(12) << "==>Expected : XII" << endl;
-    cout << ss.intToRoman(123) << "==>Expected: CXXIII" << endl;    
+    vector <int> vec;
+    vec.push_back(1);
+    vec.push_back(2);    
+    cout << vec.back() << endl;
+    vec.pop_back();
+    cout << vec.back() << endl;    
+    // cout << vec.pop_back() << endl;
     return 0;
 }
 
