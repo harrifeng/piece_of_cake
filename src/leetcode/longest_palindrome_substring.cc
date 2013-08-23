54eb73c6773f2c06c16bda23d71d0c6ab661d44f
hfeng-air <harrifeng@gmail.com>
2013-08-23 19:22:17 +0800

    go on understanding [leet] longest palindrome substring

--- src/leetcode/longest_palindrome_substring.cc
+++ src/leetcode/longest_palindrome_substring.cc
@@ -1,27 +1,11 @@
-///////////////////////////////////////////////////////////////////////////////
-// 如下图 1)j是i关于id的对称 2) mx是当前已知的最右边的值（n+p[n]最大的值）
-//
-// p[j] == 7                          p[i] == 7  
-//       mx_mirror       j(2*mx -i)     id               i          mx
-// =========+----------+----------------+----------------+----------+=========
-//              765432101234567                   765432101234567            
-// 第一种情况，j的值只有7，这15个数字都被cover在[mx_mirror,id]这个区间里面，那么右边
-// 的i也能保证有7个。 因为[mx_mirror, id]和[id, mx]是对称的。
-///////////////////////////////////////////////////////////////////////////////
-// 
-//                  p[j] == 12                      p[i] >= 10
-//       mx_mirror  j(2*mx -i)         id               i        mx   
-// =========+---------+----------------+----------------+---------+=========
-//        21X9876543210123456789X12           X9876543210123456789X??
-// 第二种情况，j的值有12，但是有两个是在mx_mirror之前，同样在右侧mx只能保证10之前的肯
-// 定是对称的。超过mx的部分有可能对，也有可能错。所以p[i]先赋值mx-i=10，再去比较mx+1
-// 和mx+2
-///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+// Given a string S, find the longest palindromic substring in S. You may assume //
+// that the maximum length of S is 1000, and there exists one unique longest     //
+// palindromic substring.                                                        //
+///////////////////////////////////////////////////////////////////////////////////
+
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <queue>
-#include <cstring>
 
 using namespace std;
 
@@ -33,51 +17,69 @@ class Solution {
         }
         string ret = "^";
         for (int i = 0; i < n; i++) {
-            ret+="#" + s.substr(i, 1);
+            ret.push_back('#');
+            ret.push_back(s[i]);
         }
         ret += "#$";
         return ret;
     }
-public:
+public:    
     string longestPalindrome(string s) {
-        string T = pre_process(s);
-        int n = T.length();
-        int *P = new int[n];
+        string new_s = pre_process(s);
+        int n = new_s.length();
+        int *cnt = new int[n];
         int max_pos_id = 0;
         int max_pos = 0;
+
         for (int i = 1; i < n-1; i++) {
-            // i's mirror position on the left
             int i_mirror = 2 * max_pos_id - i;
-            P[i] = (max_pos > i) ? min(max_pos-i, P[i_mirror]) : 0;
+            cnt[i] = (max_pos > i) ? min(max_pos-i, cnt[i_mirror]) : 0;
 
-            //to caculate the value by compare one by one
-            while (T[i + 1 + P[i]] == T[i - 1 - P[i]]) {
-                P[i]++;
+            while(new_s[i + 1 + cnt[i]] == new_s[i - 1 - cnt[i]]) {
+                cnt[i]++;
             }
 
-            //if you reach new most right position, replace corresponding value
-            if (i + P[i] > max_pos) {
+            if (i + cnt[i] > max_pos) {
                 max_pos_id = i;
-                max_pos = i + P[i];
+                max_pos = i + cnt[i];
             }
         }
-
         int maxlen = 0;
-        int centerIndex = 0;
-        for (int i = 1; i < n -1; i++) {
-            if (P[i] > maxlen) {
-                maxlen = P[i];
-                centerIndex = i;
+        int center = 0;
+        for (int i = 1; i < n - 1; i++) {
+            if (cnt[i] > maxlen) {
+                maxlen = cnt[i];
+                center = i;
             }
         }
-        delete[] P;
-        return s.substr((centerIndex - 1 - maxlen) / 2, maxlen);
+        delete[] cnt;
+        return s.substr((center - 1 - maxlen) / 2, maxlen);
     }
 };
 
-int main(int argc, char *argv[]) {
-    Solution* ss = new Solution();
+class TestCase
+{
+    string inp;
+    string exp;
+public:
+    TestCase(string input, string expected): inp(input), exp(expected){}
+    void test_solution(Solution ss) {
+        cout << "\n|--Test Casing------------------------------------|" << endl;
+        cout << "|--Input String: \t[" << inp << "]" << endl;
+        cout << "|--Expe String: \t[" << exp << "]" << endl;        
+        cout << "|--Output String: \t[" << ss.longestPalindrome(inp) << "]" <<endl;
+    }
+};
 
-    cout << ss->longestPalindrome("abcddcbaaabcd") << endl;
+
+int main(int argc, char *argv[])
+{
+    Solution ss;
+    TestCase t1("123122346556453231", "465564");
+    t1.test_solution(ss);
+    TestCase t2("aaaa", "aaaa");
+    t2.test_solution(ss);
     return 0;
 }
+
+
--- src/test.cc
+++ src/test.cc
@@ -10,72 +10,64 @@
 using namespace std;
 
 class Solution {
-public:
-    string pre_handle(string s) {
-        if (s.size() == 0) {
-            return s;
+    string pre_process(string s) {
+        int n = s.length();
+        if (n == 0) {
+            return "^$";
         }
-        string ret = "^#";
-        for (int i = 0; i < s.size(); ++i)
-        {
-            ret.push_back(s[i]);
+        string ret = "^";
+        for (int i = 0; i < n; i++) {
             ret.push_back('#');
+            ret.push_back(s[i]);
         }
-        ret.push_back('$');
+        ret += "#$";
         return ret;
     }
+public:    
     string longestPalindrome(string s) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        string new_s = pre_handle(s);
-
-        int max_id = 0;
+        string new_s = pre_process(s);
+        int n = new_s.length();
+        int *cnt = new int[n];
+        int max_pos_id = 0;
         int max_pos = 0;
-        int count[2002] = {0};
-        count[0] = 1;
-        int new_len = new_s.size();
 
-        for (int i = 1; i < new_len; ++i)
-        {
-            if (i < max_pos)  {
-                int shadow = 2 *max_id - i;
-                count[i] = count[shadow];
-            } else {
-                count[i] = 1;
-            }
-            int lp = i - 1;
-            int rp = i + 1;
-            while (new_s[lp] == new_s[rp]) {
-                count[i] += 2;
-                lp--;
-                rp++;
-            }
-            int most = rp - 1;
-            if (count[i] > count[max_id]) {
-                max_pos = most;
-                max_id = i;
-            }
-        }
-        int ret = 0;
-        int reti = 0;
-        
-        for (int i = 0; i < new_len; i++) {
-            // cout << i << "   \t" << new_s[i] << "   \t" << count[i] << endl;
-            if (ret < count[i]) {
-                ret = count[i];
-                reti = i;
-            }
-        }
-        int size = ((ret - 1) / 2 );
-        string final = new_s.substr(reti - size, ret);
+        for (int i = 1; i < n-1; i++) {
+            int i_mirror = 2 * max_pos_id - i;
+            cnt[i] = (max_pos > i) ? min(max_pos-i, cnt[i_mirror]) : 0;
 
-        string hh;
-        for (int i = 0; i < final.size(); i++) {
-            if (final[i] != '#') {
-                hh.push_back(final[i]);
+            while(new_s[i + 1 + cnt[i]] == new_s[i - 1 - cnt[i]]) {
+                cnt[i]++;
+            }
+
+            if (i + cnt[i] > max_pos) {
+                max_pos_id = i;
+                max_pos = i + cnt[i];
             }
         }
-        return hh;
+        int maxlen = 0;
+        int center = 0;
+        for (int i = 1; i < n - 1; i++) {
+            if (cnt[i] > maxlen) {
+                maxlen = cnt[i];
+                center = i;
+            }
+        }
+        delete[] cnt;
+        return s.substr((center - 1 - maxlen) / 2, maxlen);
+    }
+};
+
+class TestCase
+{
+    string inp;
+    string exp;
+public:
+    TestCase(string input, string expected): inp(input), exp(expected){}
+    void test_solution(Solution ss) {
+        cout << "\n|--Test Casing------------------------------------|" << endl;
+        cout << "|--Input String: \t[" << inp << "]" << endl;
+        cout << "|--Expe String: \t[" << exp << "]" << endl;        
+        cout << "|--Output String: \t[" << ss.longestPalindrome(inp) << "]" <<endl;
     }
 };
 
@@ -83,7 +75,10 @@ public:
 int main(int argc, char *argv[])
 {
     Solution ss;
-    cout << ss.longestPalindrome("123122346556453231") << endl;
+    TestCase t1("123122346556453231", "465564");
+    t1.test_solution(ss);
+    TestCase t2("aaaa", "aaaa");
+    t2.test_solution(ss);
     return 0;
 }
 
