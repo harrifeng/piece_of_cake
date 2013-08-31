0156f0f74e175c823038760b71ee82bde5bd6886
hfeng-air <harrifeng@gmail.com>
2013-08-31 16:14:59 +0800

    pass only the small

--- src/test.cc
+++ src/test.cc
@@ -3,6 +3,7 @@
 #include <vector>
 #include <climits>
 #include <cassert>
+#include <cmath>
 using namespace std;
 struct ListNode {
     ListNode* next;
@@ -17,13 +18,15 @@ public:
         ListNode *head = new ListNode(INT_MAX);
         ListNode *tmp = head;
         int index = 0;
+        // int sum = (1 << cnt) - 1;
         int sum = 0;
         while(1) {
             int pivot = INT_MAX;
             for (int i = 0; i < cnt; i++) {
                 if (lists[i] == NULL) {
-                    sum++;
-                    if (sum == cnt) {
+                    sum |= (1 << i);
+                    // sum++;
+                    if (sum == ((1<<cnt) -1)) {
                         return tmp->next;
                     }
                 } else if (lists[i]->val < pivot) {
@@ -64,20 +67,34 @@ void display(ListNode* hd) {
 
 int main(int argc, char *argv[]) {
     Solution* ss = new Solution();
-
+    
     int array1[3] = {1, 3, 5};
     ListNode* hd1 = get_list(array1, 3);
     display(hd1);
-
+    
     int array2[3] = {2, 4, 7};
     ListNode* hd2 = get_list(array2, 3);
     display(hd2);
-
+    
     vector<ListNode *> combine;
     combine.push_back(hd1);
     combine.push_back(hd2);
-
+    
     ListNode* ret = ss->mergeKLists(combine);
     display(ret);
+
+    // float sum = 0;
+    // for (int i = 0; i < 3; i++) {
+    //     sum += pow(2.0, i);
+    // }
+    // cout << sum << endl;
+    // int aaa = 7;
+    // aaa ^= (1 << 0);
+    // cout << aaa << endl;
+    // aaa ^= (1 << 1);
+    // cout << aaa << endl;
+    // aaa ^= (1 << 2);
+    // cout << aaa << endl;
+    // cout << (1 << 2) - 1 << endl;
     return 0;
 }
