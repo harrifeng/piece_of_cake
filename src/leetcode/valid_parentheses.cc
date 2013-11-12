bb3532be5edcf21431979c0ccb2782e34b43b797
hfeng-air <harrifeng@gmail.com>
2013-11-12 20:59:36 +0800

    refine the valid parentheses

--- src/leetcode/valid_parentheses.cc
+++ src/leetcode/valid_parentheses.cc
@@ -2,45 +2,49 @@
 #include <string>
 #include <vector>
 #include <iostream>
-
 using namespace std;
 
-bool is_valid_parentheses(string s) {
-    vector<char> sta;
-    if (s.size() == 0) {
-        return false;
-    }
-    sta.push_back(s[0]);
-
-    for (int i = 1; i < s.size(); ++i) {
-        if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
-            sta.push_back(s[i]);
-            continue;
+class Solution {
+public:
+    bool isValid(string s) {
+        if (s.length() == 0) {
+            return true;
         }
-        char current = sta.back();
-        if (s[i] == ')' && current != '(') {
+        
+        vector<char> sta;
+        sta.push_back(s[0]);
+        for (int i = 1; i < s.length(); i++) {
+            char current = sta.back();
+            
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                sta.push_back(s[i]);
+            }
+            
+            else if (s[i] == ')' && current != '(') {
+                return false;
+            }
+            else if (s[i] == '}' && current != '{') {
+                return false;
+            }
+            else if (s[i] == ']' && current != '[') {
+                return false;
+            } else { 
+                sta.pop_back();
+            }
+        }
+        if (!sta.empty()) {
             return false;
         }
-        if (s[i] == ']' && current != '[') {
-            return false;
-        }
-        if (s[i] == '}' && current != '{') {
-            return false;
-        }
-        sta.pop_back();
+        return true;
     }
-
-    if (sta.size() != 0) {
-        return false;
-    }
-    return true;
-}
+};
 
 int main(int argc, char *argv[]) {
     string p1 = "{{()}}";
     string p2 = "{{()(}{)}}";
-    cout << is_valid_parentheses(p1) << endl;
-    cout << is_valid_parentheses(p2) << endl;
+    Solution *ss = new Solution();
+    cout << ss->isValid(p1) << endl;
+    cout << ss->isValid(p2) << endl;
     return 0;
 }
 
