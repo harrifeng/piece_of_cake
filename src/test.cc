bffe12ad78932abd40efafff97f88bec6b41fb3f
Harri Feng <harrifeng@gmail.com>
2014-05-15 23:16:59 +0800

    own version smart pointer

--- src/test.cc
+++ src/test.cc
@@ -1,19 +1,47 @@
-#include <iostream>
-#include <ext/hash_map>
-
-
-int main(int argc, char *argv[])
-{
-    using namespace __gnu_cxx;
-    hash_map<int, int> hs;
-    hs[1] = 3;
-    hs[2] = 6;
-
-    hash_map<int, int>::iterator iter;
-
-    for (iter = hs.begin(); iter != hs.end(); iter++) {
-        std::cout << (*iter).first << std::endl;
-        std::cout << (*iter).second << std::endl;
+template<class T> class SmartPointer {
+public:
+    SmartPointer(T *ptr) {
+        ref = ptr;
+        ref_count = (unsigned*)malloc(sizeof(unsigned));
+        *ref_count = 1;
     }
-    return 0;
-}
+    SmartPointer(SmartPointer<T> & sptr) {
+        ref = sptr.ref;
+        ref_count = sptr.ref_count;
+        ++(*ref_count)
+    }
+    SmartPointer<T>& operator=(SmartPointer<T> &sptr) {
+        if (this == &sptr) {
+            return *this;
+        }
+        // If you(this) is already assigned to an object
+        // remove one reference
+        if (*ref_count > 0) {
+            remove();
+        }
+
+        // We(this) are now using sptr's value
+        ref = sptr.ref;
+        ref_count = sptr.ref_count;
+        ++(*ref_count);
+        return *this;
+    }
+    ~SmartPointer() {
+        remove();
+    }
+private:
+    // remove one ref
+    void remove() {
+        --(*ref_count);
+        if (*ref_count == 0) {
+            delete ref;
+            free(ref_count);
+            ref = NULL;
+            ref_count = NULL;
+        }
+    }
+    // point to the Object we want to manage
+    T* ref;
+    // ref_count number is sotred on the heap
+    unsigned* ref_count;
+};
